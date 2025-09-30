d3a2b31798531c37ddea1a8601a88d6211b6ac92
Toko0829 <168142380+Toko0829@users.noreply.github.com>
2025-09-30 13:34:07 +0400

    added card and get method

--- src/app/master.service.ts
+++ src/app/master.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MasterService {
+  baseUrl = 'http://localhost:3000/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getRecipes(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}recipes`);
+  }
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { MasterService } from '../master.service';
 
 @Component({
   selector: 'app-recipe-list',
   imports: [],
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
+  recipes: any[] = [];
 
+  constructor(private masterService: MasterService) {}
+
+  ngOnInit() {
+    return this.masterService.getRecipes().subscribe((data) => {
+      this.recipes = data;
+    });
+  }
 }
