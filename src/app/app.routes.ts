09f0a7359b6dda0ecc20f44e16098dbaa899dcfb
Toko0829 <168142380+Toko0829@users.noreply.github.com>
2025-09-30 13:25:30 +0400

    added service file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: RecipeListComponent }];
