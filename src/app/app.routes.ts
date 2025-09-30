b91c45986aeff8bc928064848274064a1f68e71c
Toko0829 <168142380+Toko0829@users.noreply.github.com>
2025-09-30 13:52:32 +0400

    added forms template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
-export const routes: Routes = [{ path: '', component: RecipeListComponent }];
+export const routes: Routes = [
+  { path: '', component: RecipeListComponent },
+  { path: 'add', component: RecipeFormComponent },
+];
